eef78cfa37eee20673da7487b18c96d6feef8e5b
AVEA01 <you-seshin@avail-corp.com>
2025-07-29 18:26:17 +0900

    Delete project-manager-demo directory

--- project-manager-demo/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">案件ダッシュボード</h1>
-      <p>ここに案件一覧が表示されます。</p>
-    </main>
-  );
-}
